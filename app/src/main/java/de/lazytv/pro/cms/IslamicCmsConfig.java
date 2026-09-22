a16a7195ec5addedf4d62c77294969390201ad0f
iljazsalihi95-wq <iljazsalihi95@gmail.com>
2026-09-23 01:35:30 +0200

    Add Islamic VOD CMS category configuration

---
+++ app/src/main/java/de/lazytv/pro/cms/IslamicCmsConfig.java
@@ -0,0 +1,14 @@
+package de.lazytv.pro.cms;
+
+public final class IslamicCmsConfig {
+    private IslamicCmsConfig() {}
+    public static final String SPREADSHEET_ID = "1COgz8NtU9QzKg_4BgNvykKqeHepk3oIGO-DsWXQhRsQ";
+    public static final String VOD_SHEET = "VOD";
+    public static final String YOUTUBE_SHEET = "YOUTUBE";
+    public static final String FILMS = "VOD_ISLAM_FILM";
+    public static final String SERIES = "VOD_ISLAM_SERIES";
+    public static final String LECTURES = "VOD_ISLAM_LECTURES";
+    public static final String DOCUMENTARIES = "VOD_ISLAM_DOCS";
+    public static final String KIDS = "VOD_ISLAM_KIDS";
+    public static final String[] HOME_CATEGORIES = {FILMS, SERIES, LECTURES, DOCUMENTARIES, KIDS};
+}
