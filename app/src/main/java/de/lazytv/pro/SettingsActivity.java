58923514143e5e64719d838f8110b0aeac3a2fef
iljazsalihi95-wq <iljazsalihi95@gmail.com>
2026-09-23 23:05:30 +0200

    Add functional Settings screen

---
+++ app/src/main/java/de/lazytv/pro/SettingsActivity.java
@@ -0,0 +1,11 @@
+package de.lazytv.pro;
+import android.app.*;import android.os.*;import android.widget.*;import android.content.*;import de.lazytv.pro.playlist.*;
+public class SettingsActivity extends Activity{
+ protected void onCreate(Bundle b){super.onCreate(b);setContentView(R.layout.activity_settings);
+  PlaylistStorage s=new PlaylistStorage(this);Playlist p=s.get(s.getActiveId());
+  ((TextView)findViewById(R.id.settings_active)).setText(p==null?"Asnjë playlist aktive":"Playlist aktive: "+p.getName());
+  findViewById(R.id.settings_playlists).setOnClickListener(v->startActivity(new Intent(this,PlaylistManagerActivity.class)));
+  findViewById(R.id.settings_activation).setOnClickListener(v->startActivity(new Intent(this,de.lazytv.pro.activation.ActivationActivity.class)));
+  findViewById(R.id.settings_back).setOnClickListener(v->finish());
+ }
+}
