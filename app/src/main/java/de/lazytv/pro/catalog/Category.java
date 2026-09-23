ea6ca2b3192b12dc5f64a733edfcc37a0caf2166
iljazsalihi95-wq <iljazsalihi95@gmail.com>
2026-09-23 23:54:04 +0200

    Make catalog categories cacheable

--- app/src/main/java/de/lazytv/pro/catalog/Category.java
+++ app/src/main/java/de/lazytv/pro/catalog/Category.java
@@ -1,5 +1,5 @@
 package de.lazytv.pro.catalog;
-public class Category {
+public class Category implements java.io.Serializable {
  public final String id,name,country; public final CatalogType type;
  public Category(String id,String name,CatalogType type){this(id,name,type,"");}
  public Category(String id,String name,CatalogType type,String country){this.id=id;this.name=name;this.type=type;this.country=country==null?"":country.trim();}
